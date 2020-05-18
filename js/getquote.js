cb9eed582939d1f3c2cda17aa0be6a7458591672
luckielogic <53406824+luckielogic@users.noreply.github.com>
2020-05-17 22:31:26 -0700

    Update getquote.js

--- js/getquote.js
+++ js/getquote.js
@@ -8,11 +8,11 @@ function getQuote() {
   }, {
     quote: "Sometimes my owners allow me to jump on their bed. That means that when they are not looking, I can allow myself to jump on the bed."
   }, {
-    quote: "My owners tell me not to eat grass. That means that I can eat grass when they're not looking. "
+    quote: "My owners tell me not to eat grass. That means that I can eat grass when they are not looking. "
   }, {
     quote: "I don't play with my toys unless my owners have guests over. It makes my owners look like they don't buy me enough toys. "
   }, {
-    quote: "I really want the balls that other dogs have, but after my owner buys them for me, I don't want them anymore. "
+    quote: "I really want the special balls that other dogs have, but after my owner buys them for me, I don't want them anymore. "
   }, {
     quote: "I sleep with my tongue out, but if you try to take a picture of me, I'll reset to a normal face. "
   }, {
@@ -30,13 +30,13 @@ function getQuote() {
   }, {
     quote: "I spend lots of time looking for the steepest hill and tallest grass to poop on. This makes it more fun for my owner to pick up."
   }, {
-    quote: "If we hike together, I will bark a lot to show you which direction we all need to be going. (I am in charge.)"
+    quote: "If we hike together, I will bark very loudly to show which direction we all need to be going. (I am in charge.)"
   }, {
     quote: "I can frequently be heard sighing deeply from the other side of the room, contemplating my past life and other thoughtful subjects."
   }, {
     quote: "I know many tricks, but I only want to do them if there is food."
   }, {
-    quote: "I don't really want to swim out to get the stick. I will let other dogs do that, then steal the stick."
+    quote: "I don't really want to swim out to get the stick. I will let other dogs do that, then steal the stick on the shore."
   }, {
     quote: "I like to be part of the husky pack at the dog park, but they think I look like a lab."
   }, {
