436b0f0020650887293368d3ce67f49935cc9658
luckielogic <53406824+luckielogic@users.noreply.github.com>
2020-05-18 22:37:21 -0700

    Update randomBackground.js

--- js/randomBackground.js
+++ js/randomBackground.js
@@ -5,7 +5,7 @@ function randomBackground()
 {
 //var num = Math.ceil( Math.random() * totalCount );
 //document.body.background = 'bgimages/'+num+'.jpg';
-  document.body.style.backgroundImage = "url('https://github.com/luckielogic/luckielogic.github.io/blob/master/js/bgimages/1.jpg')";
+  document.body.background = 'http://i.imgur.com/UI7WfVy.gif';
 document.body.style.backgroundRepeat = "no-repeat";// Background repeat
 }
 
