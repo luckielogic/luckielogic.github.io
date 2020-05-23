2776be37f7c59cf5cbf27cb2651ee7101f8b1e3b
luckielogic <53406824+luckielogic@users.noreply.github.com>
2020-05-23 13:50:52 -0700

    Update randomBackground.js

--- js/randomBackground.js
+++ js/randomBackground.js
@@ -4,6 +4,7 @@ var images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg'];
 
 $('html').css('background-color', '#2b2b2b');  
 $('html').css({'background-image': 'url(bgimages/' + images[Math.floor(Math.random() * images.length)] + ')'});
+$('html').css('background-position', 'center center');
 $('html').css('background-repeat', 'no-repeat');
 $('html').css('background-size', 'cover');
   
